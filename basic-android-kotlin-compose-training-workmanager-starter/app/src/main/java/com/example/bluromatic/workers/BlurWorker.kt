0c66c467086c1022b7cf00c3b55fbe29a3d9dce6
JuanCharlitossLopez <juancalopez825@gmail.com>
2024-02-08 11:29:16 -0600

    Terminacion del Cap. 8

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -3,7 +3,12 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.util.Log
 import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import kotlinx.coroutines.delay
 
 private const val TAG = "BlurWorker"
 
@@ -14,25 +19,32 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
         )
+        return withContext(Dispatchers.IO) {
+            // This is an utility function added to emulate slower work.
+            delay(DELAY_TIME_MILLIS)
+            //return try
+            return@withContext try {
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
 
-        return try {
-            val picture = BitmapFactory.decodeResource(
-                applicationContext.resources,
-                R.drawable.android_cupcake
-            )
-
-            val output = blurBitmap(picture, 1)
-            // Write bitmap to a temp file
-            val outputUri = writeBitmapToFile(applicationContext, output)
-            makeStatusNotification(
-                "Output is $outputUri",
-                applicationContext
-            )
-
-
-            Result.success()
-        } catch (throwable: Throwable) {
-            Result.failure()
+                val output = blurBitmap(picture, 1)
+                // Write bitmap to a temp file
+                val outputUri = writeBitmapToFile(applicationContext, output)
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
         }
     }
 }
