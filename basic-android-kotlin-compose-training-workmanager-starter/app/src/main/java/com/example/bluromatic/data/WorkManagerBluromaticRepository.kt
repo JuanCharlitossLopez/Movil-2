fcf829738ca5decf14a801de4ae9b11d43e38f77
JuanCharlitossLopez <juancalopez825@gmail.com>
2024-02-08 15:31:11 -0600

    tenimado el Cap. 10 Datos de entrada y datos de salida

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -24,16 +24,17 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
 import com.example.bluromatic.workers.BlurWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
+    private var imageUri: Uri = context.getImageUri() // <- Add this
 
     // New code
     private val workManager = WorkManager.getInstance(context)
-
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
     /**
@@ -43,6 +44,8 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
     override fun applyBlur(blurLevel: Int) {
         // Create WorkRequest to blur the image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+        // New code for input data object
+        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
         // Start the work
         workManager.enqueue(blurBuilder.build())
     }
--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -3,17 +3,24 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
+import androidx.work.workDataOf
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import kotlinx.coroutines.delay
 
 private const val TAG = "BlurWorker"
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
+        // ADD THESE LINES
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
 
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
@@ -24,19 +31,36 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
             delay(DELAY_TIME_MILLIS)
             //return try
             return@withContext try {
-                val picture = BitmapFactory.decodeResource(
+
+                // NEW code
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
+                /*val picture = BitmapFactory.decodeResource(
                     applicationContext.resources,
                     R.drawable.android_cupcake
-                )
+                )*/
 
-                val output = blurBitmap(picture, 1)
+                val resolver = applicationContext.contentResolver
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
+                )
+                val output = blurBitmap(picture, blurLevel)
+                //val output = blurBitmap(picture, 1)
                 // Write bitmap to a temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
-                makeStatusNotification(
-                    "Output is $outputUri",
-                    applicationContext
-                )
-                Result.success()
+                //makeStatusNotification(
+                //   "Output is $outputUri",
+                //    applicationContext
+                //)
+
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+                //Result.success()
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
