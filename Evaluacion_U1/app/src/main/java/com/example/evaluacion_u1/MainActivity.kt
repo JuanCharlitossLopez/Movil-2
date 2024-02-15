36a2cbe1e80bf180e5c2d9666e21b56eb81d5a49
JuanCharlitossLopez <juancalopez825@gmail.com>
2024-02-14 21:33:55 -0600

    Avance de Evaluacion: Agregamos el cuadro de texto para ingresar la contraseña del usuario

--- Evaluacion_U1/app/src/main/java/com/example/evaluacion_u1/MainActivity.kt
+++ Evaluacion_U1/app/src/main/java/com/example/evaluacion_u1/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -20,11 +25,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.evaluacion_u1.ui.theme.Evaluacion_U1Theme
@@ -35,7 +44,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Evaluacion_U1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     //Greeting("Android")
                     Usuario()
                 }
@@ -47,8 +59,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
@@ -61,25 +73,46 @@ fun GreetingPreview() {
 }
 
 @Composable
-fun Usuario(){
+fun Usuario() {
     Column(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
-        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Imagen de usuario",
-            modifier = Modifier.size(80.dp))
-        var text by remember {mutableStateOf("")}
-        OutlinedTextField(value = text, onValueChange = {newText ->
-            text = newText
-        },
-            label = { Text(text = "Ingresa tu Matricula")},
+    ) {
+        Icon(
+            imageVector = Icons.Default.AccountCircle, contentDescription = "Imagen de usuario",
+            modifier = Modifier.size(80.dp)
+        )
+        var user by remember { mutableStateOf("") }
+        OutlinedTextField(
+            modifier = Modifier.padding(10.dp),
+            value = user, onValueChange = { newText ->
+                user = newText
+            },
+            label = { Text(text = "Ingresa tu Matricula") },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text,
                 imeAction = ImeAction.Done
             )
         )
+        var password by rememberSaveable { mutableStateOf("") }
+        var passwordVer by remember { mutableStateOf(false) }
+        val icon = if(passwordVer){Icons.Default.Favorite}else{Icons.Default.FavoriteBorder}
+        OutlinedTextField(value = password, onValueChange = { password = it },
+            label = { Text(text = "Password")},
+            trailingIcon = {
+                IconButton(onClick = { passwordVer = !passwordVer }) {
+                    Icon(imageVector = icon, contentDescription = "Ver password")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if(passwordVer){VisualTransformation.None}else{
+                PasswordVisualTransformation()
+            }
+            )
     }
 }
 
