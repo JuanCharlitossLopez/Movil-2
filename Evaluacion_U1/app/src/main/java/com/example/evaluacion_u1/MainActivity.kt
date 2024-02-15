249ea41f204ac67e2c185ad2940b3d5950782fda
JuanCharlitossLopez <juancalopez825@gmail.com>
2024-02-14 21:07:03 -0600

    Avance en Evaluacion: Agregaron la caja de texto para ingresar el usuario

--- Evaluacion_U1/app/src/main/java/com/example/evaluacion_u1/MainActivity.kt
+++ Evaluacion_U1/app/src/main/java/com/example/evaluacion_u1/MainActivity.kt
@@ -3,19 +3,30 @@ package com.example.evaluacion_u1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.evaluacion_u1.ui.theme.Evaluacion_U1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +36,8 @@ class MainActivity : ComponentActivity() {
             Evaluacion_U1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    Usuario()
                 }
             }
         }
@@ -45,6 +57,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Evaluacion_U1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun Usuario(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Imagen de usuario",
+            modifier = Modifier.size(80.dp))
+        var text by remember {mutableStateOf("")}
+        OutlinedTextField(value = text, onValueChange = {newText ->
+            text = newText
+        },
+            label = { Text(text = "Ingresa tu Matricula")},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
+        )
     }
 }
 
